e0ea612dfa08dfd3ae6c241e5dc335fbf0e7302e
Jan Schulte <jan@work-with-jan.com>
2021-03-01 16:27:29 -0500

    validate field presence
    and also check for seo_description

--- src/frontmatter.ts
+++ src/frontmatter.ts
@@ -9,11 +9,21 @@ export default class FrontmatterAnalyzer {
 
     public analyze(keyword: string) : Array<AnalyzerResult> {
         const frontmatter = matter(this.markdownFile);
-        const { title } = frontmatter.data;
+        const { seo_title, seo_description } = frontmatter.data;
         const results = [];
-        if (title.indexOf(keyword) === -1) {
-            results.push(new AnalyzerResult('title', 'missing keyword'));
+        if(!seo_title) {
+            results.push(new AnalyzerResult('seo_title', 'not found'));
         }
+        if (seo_title && seo_title.indexOf(keyword) === -1) {
+            results.push(new AnalyzerResult('seo_title', `Keyword '${keyword}' not found`));
+        }
+        if(!seo_description) {
+            results.push(new AnalyzerResult('seo_description', 'not found'));
+        }
+        if (seo_description && seo_description.indexOf(keyword) === -1) {
+            results.push(new AnalyzerResult('seo_title', `Keyword '${keyword}' not found`));
+        }
+
         return results;
     }
 }
