3d9dbadc7e4e99234bb4985cb34ba9a502b0400d
Jan Schulte <jan@work-with-jan.com>
2021-03-01 20:22:08 -0500

    use mocha describe for structuring

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-
+import { describe } from 'mocha';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
@@ -8,29 +8,30 @@ import extractKeywords from '../../keywords';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
-	const markdown = `---
+	describe('keywords.ts', () => {
+		const markdown = `---
 Keywords:
-  - Foo
-  - Bar
+- Foo
+- Bar
 seo_title: This is about seo
 seo_description: Learn how to seo perfectly
 ---
 # Foo`;
 
-	const markdownNoKeywords = `---
+		const markdownNoKeywords = `---
 seo_title: This is about seo
 seo_description: Learn how to seo perfectly
 ---
 # Foo`;
 
+		test('extract keywords from frontmatter', () => {
+			const keywords = extractKeywords(markdown);
+			assert.notStrictEqual(['Foo', 'Bar'], keywords);
+		});
 
-	test('extract keywords from frontmatter', () => {
-		const keywords = extractKeywords(markdown);
-		assert.notStrictEqual(['Foo', 'Bar'], keywords);
-	});
-
-	test('returns empty list if no keywords are found', () => {
-		const keywords = extractKeywords(markdownNoKeywords);
-		assert.notStrictEqual([], keywords);
+		test('returns empty list if no keywords are found', () => {
+			const keywords = extractKeywords(markdownNoKeywords);
+			assert.notStrictEqual([], keywords);
+		});
 	});
 });
