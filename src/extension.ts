1e59ba5cf02daa28af9846241e4eb9a93f516f50
Jan Schulte <jan@work-with-jan.com>
2021-03-23 21:10:23 -0400

    Move the cursor to the clicked finding with position

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,43 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import TreeProvider from './treeProvider';
+import TreeProvider, { FindingWithPosition } from './treeProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	const seoResultTreeProvider = new TreeProvider();
-	vscode.window.registerTreeDataProvider('seo-results', seoResultTreeProvider);
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    const seoResultTreeProvider = new TreeProvider();
+    const treeView = vscode.window.createTreeView('seo-results', {
+        treeDataProvider: seoResultTreeProvider
+    });
 
-	let disposable = vscode.commands.registerCommand('better-seo.refresh', () => {
-		seoResultTreeProvider.refresh();
-	});
-	context.subscriptions.push(disposable);
+    treeView.onDidChangeSelection((event) => {
+        console.log(event instanceof FindingWithPosition);
+        if(event.selection.length === 0) {
+            return;
+        }
+
+        const firstSelection = event.selection[0];
+
+        if(firstSelection instanceof FindingWithPosition) {
+            const editor = vscode.window.activeTextEditor;
+            if(!editor) {
+                return;
+            }
+            const position = editor.selection.active;
+            const newPosition = position.with(firstSelection.location.start.line - 1, firstSelection.location.start.column);
+            const newSelection = new vscode.Selection(newPosition, newPosition);
+            editor.selection = newSelection;
+        }
+    });
+
+    let disposable = vscode.commands.registerCommand('better-seo.refresh', () => {
+        seoResultTreeProvider.refresh();
+    });
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
