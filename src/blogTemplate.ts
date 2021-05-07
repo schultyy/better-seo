86ef90f6d6c75b57bcbcac7d5e78a2c33f54edd7
Jan Schulte <jan@work-with-jan.com>
2021-05-06 21:53:23 -0400

    Add Step by Step Template

--- src/blogTemplate.ts
+++ src/blogTemplate.ts
@@ -1,10 +1,19 @@
+export function options() {
+    return [
+        "List Post",
+        "Step by Step",
+        "Expanded Definition",
+        "Beginners Guide"
+    ];
+}
+
 export function fromOption(userOption: string) : string | null {
     switch(userOption) {
         case "List Post": {
             return new ListPost().render();
         }
         case "Step by Step": {
-
+            return new StepByStepGuide().render()
         }
         case "Expanded Definition": {
 
@@ -15,6 +24,53 @@ export function fromOption(userOption: string) : string | null {
         default: {
             return null;
         }
+    }
+}
+
+class StepByStepGuide {
+    private headline() {
+        return "# How to set up a Blogging Framework with VSCode in three Easy Steps"
+        + "\n<small>\n"
+        + "Use the 'How' Format\n"
+        + "Mention the _desired outcome_\n"
+        + "List the number of steps\n"
+        + "</small>\n";
+    }
+
+    private intro() {
+        return "**Looking to start your own blog to share your tech lessons learned?"
+        + " The trick is a good setup that doesn't cause any overhead.**"
+        + "\n"
+        + "\nThis is the approach I'm using to produce high-quality blog posts on a frequent schedule with focusing on my content exclusively\n"
+        + "\n----\n"
+        + "\n"
+        + "Explanation:\n\n"
+        + "- Show that you understand the **problem**\n"
+        + "- Present the **solution** - briefly\n"
+        + "- Show **proof** you have the solution\n";
+    }
+
+    private listItems() {
+        return "## Step #1. Install the right plugins\n"
+        + "\n- Use H2 Subheadings"
+        + "\n- **Number** them"
+        + "\n- Start each one with a **present-tense verb**"
+        + "\n\nYour workflow is only as good as the plugins you use to write great content. Let's explore which plugins are the most helpful.\n"
+    }
+
+    private conclusion() {
+        return "## Final Thoughts\n"
+        + "\n - Summarize **key points**"
+        + "\n - Keep it short"
+    }
+
+    render() {
+        return [
+            this.headline(),
+            this.intro(),
+            this.listItems(),
+            this.conclusion()
+        ].join("\n")
     }
 }
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { fromOption } from './blogTemplate';
+import { fromOption, options } from './blogTemplate';
 import TreeProvider, { FindingWithPosition } from './treeProvider';
 
 function moveCursor(firstSelection: FindingWithPosition) {
@@ -45,7 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     let disposableTemplateCommand = vscode.commands.registerCommand('better-seo.new-post', async () => {
         try {
-            const userChoice = await vscode.window.showQuickPick(["List Post", "Tutorial"], {
+            const userChoice = await vscode.window.showQuickPick(options(), {
                 canPickMany: false
             });
 
